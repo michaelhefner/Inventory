fe9b2a61fd456a730967ed9fb58c953ec3127c54
michael <michael.hefner@outlook.com>
2020-04-21 22:40:21 -0400

    created event emitter for signed in buttons

---
+++ src/app/nav/navbar/navbar.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../auth/auth.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.sass']
+})
+export class NavbarComponent implements OnInit {
+
+  isSignedIn: boolean;
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.authService.listener().subscribe(res => this.isSignedIn = res);
+
+  }
+
+  SignIn() {
+    this.router.navigate(['signin']);
+  }
+
+  SignOut() {
+    console.log('Sign Out');
+    this.authService.signOut();
+  }
+
+}
